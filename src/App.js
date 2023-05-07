c4d08864bd68701f05701090d91e57a1b2263760
Nestor Vega <vega.nestor1@gmail.com>
2023-05-06 20:35:42 -0700

    apple images included

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './app.css'
 
 function App() {
   return (
+    <>
     <div className="App">
       {/* <header className="appleHeader"></header> */}
       <nav className='navBarApple'>
+        <a className='navBarText'>
+          <img src='../images/appleicon.png' alt='Apple Icon'/>
+        </a>
         <a className='navBarText'>Store</a>
         <a className='navBarText'>Mac</a>
         <a className='navBarText'>iPad</a>
@@ -17,6 +21,16 @@ function App() {
         <a className='navBarText'>Support</a>
       </nav>
     </div>
+      <div className='sectionBackground'>
+        {/* <img src='../images/iphoneBackground.png'/> */}
+        <h1 className='imageText'>
+          iPhone 14 Pro
+        </h1>
+        <h3>
+          Pro. Beyond.
+        </h3>
+      </div>
+    </>
   );
 }
 
